f8ad7ab2a8f110e9a37eb3c109760ff676be4168
Julian Phillips <julian.p@me.com>
2019-11-19 14:26:57 -0500

    feat: Installed axios, created Footer component, logged basic functions for working with Tzikas/MONGO_REST.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 // Components
 import Nav from "./Components/Nav/Nav";
@@ -8,6 +9,31 @@ import Hero from "./Components/Hero/Hero";
 import "./App.css";
 
 function App() {
+  // Create the collection
+  // axios
+  //   .post("https://ironrest.herokuapp.com/createCollection/winningTransit")
+  //   .then(res => {
+  //     console.log(res.data);
+  //   });
+
+  // Post to the collection
+  // axios
+  //   .post("https://ironrest.herokuapp.com/winningTransit", {
+  //     testing: "Winning"
+  //   })
+  //   .then(res => {
+  //     console.log(res.data);
+  //   });
+
+  // Delete an item from the collection.
+  // axios
+  //   .delete(
+  //     "https://ironrest.herokuapp.com/winningTransit/5dd43f3e7b55290017a2b1aa"
+  //   )
+  //   .then(res => {
+  //     console.log(res.data);
+  //   });
+
   return (
     <div className="App">
       <Nav />
