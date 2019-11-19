20e949c0d9cb167a6a9f07451fb48ab667b6e7b6
Julian Phillips <julian.p@me.com>
2019-11-19 11:55:21 -0500

    chore: Removed stock resources. feat: Started stylizing the Nav.

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -3,8 +3,15 @@ import React from "react";
 import "./Nav.css";
 export default function Nav() {
   return (
-    <div>
-      <h1>Insert NavBar here</h1>
+    <div className="nav-container">
+      <nav className="nav-content">
+        <img
+          className="nav-icon"
+          src="/icons/train_icon.png"
+          alt="train icon"
+        />
+        <h1>Insert NavBar here</h1>
+      </nav>
     </div>
   );
 }
