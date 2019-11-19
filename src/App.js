74c53a0e62f13ee6337ab3374b3da499c88f8319
Julian Phillips <julian.p@me.com>
2019-11-19 12:22:22 -0500

    feat: Added Hero component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 // Components
 import Nav from "./Components/Nav/Nav";
+import Hero from "./Components/Hero/Hero";
 
 // Styling
 import "./App.css";
@@ -10,12 +11,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <h1>Welcome to WinningTransit</h1>
-      <p>
-        There are many factors in life that are uncontrollable but the quality
-        of your train car does not have to be one of them. Be a part of our
-        community and learn to choose wisely on your next ride.
-      </p>
+      <Hero />
     </div>
   );
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <h1 className="hero-title">Welcome to WinningTransit</h1>
+      <p className="hero-text">
+        There are many factors in life that are uncontrollable but the quality
+        of your train car does not have to be one of them. Be a part of our
+        community and learn to choose wisely on your next ride.
+      </p>
+    </div>
+  );
+}
