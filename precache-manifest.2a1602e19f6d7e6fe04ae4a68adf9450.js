20c2d207da730d0bb7147a157c1b248df2bc698e
Julian Phillips <julian.p@me.com>
2019-11-20 14:41:48 -0500

    Updates

---
+++ precache-manifest.2a1602e19f6d7e6fe04ae4a68adf9450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5485008ccc69575aae5114494e0f64e",
+    "url": "/Winning-Transit/index.html"
+  },
+  {
+    "revision": "0d64718ebd8a6d357314",
+    "url": "/Winning-Transit/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "37bdf5724c3a2d248cd3",
+    "url": "/Winning-Transit/static/css/main.ce39b0a9.chunk.css"
+  },
+  {
+    "revision": "0d64718ebd8a6d357314",
+    "url": "/Winning-Transit/static/js/2.78c436e0.chunk.js"
+  },
+  {
+    "revision": "37bdf5724c3a2d248cd3",
+    "url": "/Winning-Transit/static/js/main.52309e46.chunk.js"
+  },
+  {
+    "revision": "2f567135451562428d66",
+    "url": "/Winning-Transit/static/js/runtime-main.de4a5208.js"
+  }
+]);
