06edb8fbf0537716dbb4ba105accb110dc4b36b0
Julian Phillips <julian.p@me.com>
2019-11-21 11:06:21 -0500

    feat: Access to each station's trains loaded into variables.

--- src/Components/Station/Station.jsx
+++ src/Components/Station/Station.jsx
@@ -8,41 +8,53 @@ import Axios from "axios";
 export default class Station extends Component {
   state = {
     station: {},
-    northTrain1: {},
-    northTrain2: {},
-    northTrain3: {},
-    southTrain1: {},
-    southTrain2: {},
-    southTrain3: {}
+    trains: {
+      northTrain1: { name: "NB_Time1_Train" },
+      northTrain2: { name: "NB_Time2_Train" },
+      northTrain3: { name: "NB_Time3_Train" },
+      southTrain1: { name: "SB_Time1_Train" },
+      southTrain2: { name: "SB_Time2_Train" },
+      southTrain3: { name: "SB_Time3_Train" }
+    }
   };
   componentDidMount() {
     this.updateStation();
-    // this.updateTrains();
   }
 
-  updateStation = () => {
-    Axios.get(
+  updateStation = async () => {
+    let result = await Axios.get(
       `https://miami-transit-api.herokuapp.com/api/TrainTracker.json?StationID=${this.props.StationID}`
-    ).then(res => {
-      this.setState({ station: res.data.RecordSet.Record });
+    );
+    this.setState({ station: result.data.RecordSet.Record }, () => {
+      this.updateTrains();
     });
   };
-  updateTrains = () => {
-    Axios.get(
-      `http://miami-transit-api.herokuapp.com/api/Trains.json?TrainID=${this.state.station.NB_Time1_Train}`
-    ).then(res => {
-      console.log(this.state);
-      console.log(res);
-      //   this.setState({ northTrain1: res });
+
+  updateTrains = prop => {
+    let names = this.state.trains;
+
+    Object.keys(names).map(async key => {
+      // console.log(key);
+      let result = await Axios.get(
+        `http://miami-transit-api.herokuapp.com/api/Trains.json?TrainID=${
+          this.state.station[this.state.trains[key].name]
+        }`
+      );
+
+      console.log("did this happen?");
+      console.log(key);
+      console.log(result);
+      // this.setState({
+      //   trains: { ...this.state.trains, [key]: result.data.RecordSet }
+      // });
     });
   };
+
   render() {
     if (this.props.StationID !== this.state.station.StationID) {
       this.updateStation();
-      //   if (this.props.StationID === this.state.station.StationID) {
-      //     this.updateTrains();
-      //   }
     }
+
     return (
       <div>
         <Ride {...this.props} allStations={this.props.allStations} />
