1b1eedf3000b6acd40cddfa3ce66a81fc5bee68e
Julian Phillips <julian.p@me.com>
2019-11-21 20:54:41 -0500

    feat: generateTrains now prints north and south trains.

--- src/Components/Station/Station.jsx
+++ src/Components/Station/Station.jsx
@@ -9,16 +9,37 @@ export default class Station extends Component {
   state = {
     station: {},
     trains: {
-      northTrain1: { name: "NB_Time1_Train", data: {}, cars: {} },
-      northTrain2: { name: "NB_Time2_Train", data: {}, cars: {} },
-      northTrain3: { name: "NB_Time3_Train", data: {}, cars: {} },
-      southTrain1: { name: "SB_Time1_Train", data: {}, cars: {} },
-      southTrain2: { name: "SB_Time2_Train", data: {}, cars: {} },
+      northTrain1: {
+        name: "NB_Time1_Train",
+        data: {},
+        cars: {}
+      },
+      northTrain2: {
+        name: "NB_Time2_Train",
+        data: {},
+        cars: {}
+      },
+      northTrain3: {
+        name: "NB_Time3_Train",
+        data: {},
+        cars: {}
+      },
+      southTrain1: {
+        name: "SB_Time1_Train",
+        data: {},
+        cars: {}
+      },
+      southTrain2: {
+        name: "SB_Time2_Train",
+        data: {},
+        cars: {}
+      },
       southTrain3: { name: "SB_Time3_Train", data: {}, cars: {} }
     }
   };
   componentDidMount() {
     this.updateStation();
+    // this.generateTrainList();
   }
 
   updateStation = async () => {
@@ -73,36 +94,53 @@ export default class Station extends Component {
     let northTrains = [];
     let southTrains = [];
 
-    console.log(allTrainKeys);
     allTrainKeys.forEach(train => {
       if (train.startsWith("north") === true) {
-        console.log("north Train!", northTrains, this.state.trains[train]);
         northTrains.push(this.state.trains[train]);
-        console.log(northTrains);
       }
       if (train.startsWith("south") === true) {
-        console.log("south Train!");
-
         southTrains.push(this.state.trains[train]);
-
-        console.log(southTrains);
       }
     });
-    // this.generateTrains(northTrains, southTrains);
+    return this.generateTrains(northTrains, southTrains);
+  };
+
+  generateTrains = (north, south) => {
+    let northTrains = north.map((train, i) => {
+      if (train.data.Record != null) {
+        return <h2 key={i}>{train.data.Record.TrainID}</h2>;
+      } else {
+        return <h2 key={i}>Data unavailable</h2>;
+      }
+    });
+    let southTrains = south.map((train, i) => {
+      if (train.data.Record != null) {
+        return <h2 key={i}>{train.data.Record.TrainID}</h2>;
+      } else {
+        return <h2 key={i}>Data unavailable</h2>;
+      }
+    });
+    let allTrains = (
+      <div>
+        <div className="north-bound-container">{northTrains}</div>
+        <div className="south-bound-container">{southTrains}</div>
+      </div>
+    );
+    // console.log(northTrains);
+    return allTrains;
   };
 
   render() {
     if (this.props.StationID !== this.state.station.StationID) {
       this.updateStation();
-      this.generateTrainList();
+      // this.generateTrainList();
     }
 
     return (
       <div>
         <Ride {...this.props} allStations={this.props.allStations} />
         <h1>Welcome to {this.state.station.StationName} Station</h1>
-        <div className="north-bound-container"></div>
-        <div className="south-bound-container"></div>
+        <div>{this.generateTrainList()}</div>
       </div>
     );
   }
